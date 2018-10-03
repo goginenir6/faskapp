b5fe65acf8aee9986c0d8c2abb6bbb806145ec0a
ravikumar <goginenir6@gmail.com>
2018-10-03 07:55:23 +0530

    Initial templates settings and static files configure

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
+
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Ravikumar',
+        'title': 'Blog post 1',
+        'content': 'Hi This is first blog',
+        'date_posed': 'oct 2, 2018'
+    },
+    {
+        'author': 'anilkumar',
+        'title': 'Blog post 2',
+        'content': 'Hi This is secnd blog',
+        'date_posed': 'oct 3, 2018'
+    }
+]
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/home')
+def home():
+    return render_template ('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template ('about.html', title='About')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
