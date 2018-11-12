8ae9d9e78cb39fe8efa06a11b5b9251e0df841c7
Gogineni Ravikumar (Partner) <gogineni.p.ravikumar@happiestminds.com>
2018-11-12 13:30:49 +0530

    flask-3 forms pages register and login

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import Registrationform, Loginform
 
 
 app = Flask(__name__)
+
+# app.config['SECRET_KEY'] = 'd5fa3e4e3383c1be339ef4845360ec14'
 
 
 posts = [
@@ -12,6 +15,24 @@ posts = [
         'date_posed': 'oct 2, 2018'
     },
     {
+        'author': 'anilkumar',
+        'title': 'Blog post 2',
+        'content': 'Hi This is secnd blog',
+        'date_posed': 'oct 3, 2018'
+    },
+     {
+        'author': 'anilkumar',
+        'title': 'Blog post 2',
+        'content': 'Hi This is secnd blog',
+        'date_posed': 'oct 3, 2018'
+    },
+     {
+        'author': 'anilkumar',
+        'title': 'Blog post 2',
+        'content': 'Hi This is secnd blog',
+        'date_posed': 'oct 3, 2018'
+    },
+     {
         'author': 'anilkumar',
         'title': 'Blog post 2',
         'content': 'Hi This is secnd blog',
@@ -27,6 +48,17 @@ def home():
 @app.route('/about')
 def about():
     return render_template ('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = Registrationform()
+    return render_template ('register.html', title='register user', form = form)
+
+@app.route('/login')
+def login():
+    form = Loginform()
+    return render_template ('login.html', title='Login user', form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
